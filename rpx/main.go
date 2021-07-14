58ac5ba4fa032a96c0a5a79597237f2b1e65c86d
lianghuigu(古良辉) <lianghuigu@tencent.com>
2021-07-14 16:35:22 +0800

    day 7

--- rpx/main.go
+++ rpx/main.go
@@ -1,70 +1,53 @@
 package main
 
+/*
+$ curl "http://localhost:9999"
+Hello Geektutu
+$ curl "http://localhost:9999/panic"
+{"message":"Internal Server Error"}
+$ curl "http://localhost:9999"
+Hello Geektutu
+
+>>> log
+2020/01/09 01:00:10 Route  GET - /
+2020/01/09 01:00:10 Route  GET - /panic
+2020/01/09 01:00:22 [200] / in 25.364µs
+2020/01/09 01:00:32 runtime error: index out of range
+Traceback:
+        /usr/local/Cellar/go/1.12.5/libexec/src/runtime/panic.go:523
+        /usr/local/Cellar/go/1.12.5/libexec/src/runtime/panic.go:44
+        /Users/7days-golang/day7-panic-recover/main.go:47
+        /Users/7days-golang/day7-panic-recover/gee/context.go:41
+        /Users/7days-golang/day7-panic-recover/gee/recovery.go:37
+        /Users/7days-golang/day7-panic-recover/gee/context.go:41
+        /Users/7days-golang/day7-panic-recover/gee/logger.go:15
+        /Users/7days-golang/day7-panic-recover/gee/context.go:41
+        /Users/7days-golang/day7-panic-recover/gee/router.go:99
+        /Users/7days-golang/day7-panic-recover/gee/gee.go:130
+        /usr/local/Cellar/go/1.12.5/libexec/src/net/http/server.go:2775
+        /usr/local/Cellar/go/1.12.5/libexec/src/net/http/server.go:1879
+        /usr/local/Cellar/go/1.12.5/libexec/src/runtime/asm_amd64.s:1338
+
+2020/01/09 01:00:32 [500] /panic in 395.846µs
+2020/01/09 01:00:38 [200] / in 6.985µs
+*/
+
 import (
-	"fmt"
-	"image"
-	"image/color"
-	"image/gif"
-	"io"
-	"log"
-	"math"
-	"math/rand"
 	"net/http"
-	"sync"
+
+	"web-cache-rpc/rpx/gee"
 )
-
-var palette = []color.Color{color.White, color.Black}
-
-const (
-	whiteIndex = 0 // first color in palette
-	blackIndex = 1 // next color in palette
-)
-
-var mu sync.Mutex
-var count int
 
 func main() {
-	hanlder := func(w http.ResponseWriter, r *http.Request) {
-		lissajous(w)
-	}
-	http.HandleFunc("/", hanlder) // 回声请求调用处理程序
-	// 回声请求调用处理程序
-	http.HandleFunc("/count", counter)
-	log.Fatal(http.ListenAndServe("localhost:8000", nil))
-}
+	r := gee.Default()
+	r.GET("/", func(c *gee.Context) {
+		c.String(http.StatusOK, "Hello Geektutu\n")
+	})
+	// index out of range for testing Recovery()
+	r.GET("/panic", func(c *gee.Context) {
+		names := []string{"geektutu"}
+		c.String(http.StatusOK, names[100])
+	})
 
-//counter回显目前为止调用的次数
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
-}
-
-//利萨茹图形实现函数
-func lissajous(out io.Writer) {
-	const (
-		cycles  = 5     // number of complete x oscillator revolutions
-		res     = 0.001 // angular resolution
-		size    = 100   // image canvas covers [-size..+size]
-		nframes = 64    // number of animation frames
-		delay   = 8     // delay between frames in 10ms units
-	)
-	freq := rand.Float64() * 3.0 // relative frequency of y oscillator
-	anim := gif.GIF{LoopCount: nframes}
-	phase := 0.0 // phase difference
-	for i := 0; i < nframes; i++ {
-		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
-		img := image.NewPaletted(rect, palette)
-		for t := 0.0; t < cycles*2*math.Pi; t += res {
-			x := math.Sin(t)
-			y := math.Sin(t*freq + phase)
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5),
-				blackIndex)
-		}
-		phase += 0.1
-		anim.Delay = append(anim.Delay, delay)
-		anim.Image = append(anim.Image, img)
-	}
-	gif.EncodeAll(out, &anim) // NOTE: ignoring encoding errors
+	r.Run(":9999")
 }
